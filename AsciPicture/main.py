679da41c655be330a162fa31cec3b6ed94d85717
DietmarStahl <44554617+DietmarStahl@users.noreply.github.com>
2024-11-17 23:55:12 +0000

    update

--- AsciPicture/main.py
+++ AsciPicture/main.py
@@ -1,3 +1,22 @@
-from matplotlib.image import imread
+import matplotlib.pyplot as plt
+import urllib.request
+import io
+import numpy as np
 
-image = imread('image.png')
+url = "https://github.com/DietmarStahl/Stuff/blob/main/AsciPicture/image.png?raw=true"
+response = urllib.request.urlopen(url)
+image = plt.imread(io.BytesIO(response.read()))
+
+weights = np.array([0.2989, 0.5870, 0.1140])
+image_values = np.dot(image[...,:3], weights)
+
+ascii_bins = [0.1, 0.2, 0.3, 0.4, 0.6, 0.8, 1.0]
+ascii_chars = np.array(['.', ':', '+', '*', '#', '%', '@'])
+
+bin_indices = np.digitize(image_values, ascii_bins)
+
+image_ascii = ascii_chars[bin_indices - 1]
+
+with open('ascii_art.txt', 'w') as f: 
+    for row in image_ascii:
+        f.write(''.join(row) + '\n')
